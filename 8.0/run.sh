d3fbe5fcdb7757cffe1f734bee7731709f2411d2
Aaron Bohy <aab@odoo.com>
2014-12-31 14:33:55 +0100

    [FIX] Dockerfile fixed according to Yosifkit's review
      - Clean up of APT with rm -rf /var/lib/apt/lists/*
      - Use environment variables for db_user and db_password
      - Install Odoo release of 12/31/2014

--- 8.0/run.sh
+++ 8.0/run.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# set openerp-server.conf
-echo "[options]
-; This is the password that allows database operations:
-; admin_passwd = admin
-db_host = $DB_PORT_5432_TCP_ADDR
-db_port = $DB_PORT_5432_TCP_PORT
-db_user = odoo
-db_password = odoo
-data_dir = /var/lib/odoo
-addons_path = /usr/lib/python2.7/dist-packages/openerp/addons" > /etc/odoo/openerp-server.conf
+# set variables in openerp-server.conf
+sed -i "s/^db_host.*/db_host = $DB_PORT_5432_TCP_ADDR/" /etc/odoo/openerp-server.conf
+sed -i "s/^db_port.*/db_port = $DB_PORT_5432_TCP_PORT/" /etc/odoo/openerp-server.conf
+sed -i "s/^db_user.*/db_user = $DB_ENV_POSTGRES_USER/" /etc/odoo/openerp-server.conf
+sed -i "s/^db_password.*/db_password = $DB_ENV_POSTGRES_PASSWORD/" /etc/odoo/openerp-server.conf
+sed -i "\$adata_dir = /var/lib/odoo" /etc/odoo/openerp-server.conf
 
 # start Odoo
 exec gosu odoo /usr/bin/openerp-server --config /etc/odoo/openerp-server.conf

0c90527ef908add1f125406a81519f9626d982d3
Aaron Bohy <aab@odoo.com>
2015-01-02 10:57:31 +0100

    [ADD] Configuration file template

--- 8.0/run.sh
+++ 8.0/run.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# set variables in openerp-server.conf
-sed -i "s/^db_host.*/db_host = $DB_PORT_5432_TCP_ADDR/" /etc/odoo/openerp-server.conf
-sed -i "s/^db_port.*/db_port = $DB_PORT_5432_TCP_PORT/" /etc/odoo/openerp-server.conf
-sed -i "s/^db_user.*/db_user = $DB_ENV_POSTGRES_USER/" /etc/odoo/openerp-server.conf
-sed -i "s/^db_password.*/db_password = $DB_ENV_POSTGRES_PASSWORD/" /etc/odoo/openerp-server.conf
-sed -i "\$adata_dir = /var/lib/odoo" /etc/odoo/openerp-server.conf
+CONFIG_FILE=/etc/odoo/openerp-server.conf
+
+# sets a configuration variable in openerp-server.conf
+# $1: key, $2: value
+function set_config {
+  if grep -q "^$1.*" $CONFIG_FILE
+  then
+    sed -i "s/^$1.*$/$1 = $2/" $CONFIG_FILE
+  else
+    sed -i "$ a$1 = $2" $CONFIG_FILE
+  fi
+}
+
+# set odoo data directory and database host, port, user and password
+set_config "data_dir" "\/var\/lib\/odoo"
+set_config "db_host" $DB_PORT_5432_TCP_ADDR
+set_config "db_port" $DB_PORT_5432_TCP_PORT
+set_config "db_user" $DB_ENV_POSTGRES_USER
+set_config "db_password" $DB_ENV_POSTGRES_PASSWORD
 
 # start Odoo
-exec gosu odoo /usr/bin/openerp-server --config /etc/odoo/openerp-server.conf
+exec gosu odoo /usr/bin/openerp-server --config $CONFIG_FILE

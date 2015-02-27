f000eeed23eff1e61bd255deb21b7000cac404aa
Samus CTO <cto@openerp.com>
2015-02-27 14:41:23 +0100

    [IMP] use ENTRYPOINT instead of cmd and permit giving command arguments

--- 8.0/run.sh
+++ 8.0/run.sh
@@ -6,5 +6,7 @@ export PGPORT=$DB_PORT_5432_TCP_PORT
 export PGUSER=$DB_ENV_POSTGRES_USER
 export PGPASSWORD=$DB_ENV_POSTGRES_PASSWORD
 
-# start Odoo
-exec gosu odoo /usr/bin/openerp-server --config=/etc/odoo/openerp-server.conf
+[ "$1" != "--" ] && exec "$@"
+
+shift
+exec gosu odoo /usr/bin/openerp-server --config=/etc/odoo/openerp-server.conf "$@"

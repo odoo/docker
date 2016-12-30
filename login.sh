23abdcb305a44755b631e8529d075749658e8679
Pedro Salgado <steenzout@ymail.com>
2016-12-30 14:45:59 -0700

    added bash scripts to start, stop and login to a container.

---
+++ login.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# #####
+#
+# Login to a running Odoo container.
+#
+# usage:
+#
+#   bash login.sh 9.0 20160101
+#
+# author: Pedro Salgado <steenzout@ymail.com>
+# version: 1.0
+#
+# #####
+
+
+ODOO_VERSION="$1"
+ODOO_RELEASE="$2"
+
+
+validation() {
+  : "${ODOO_VERSION:?first argument needs to be set to the Odoo version (8.0, 9.0 or 10.0).}"
+  : "${ODOO_RELEASE:?second argument needs to be set to the Odoo release (e.g. 20160101).}"
+}
+
+
+validation
+
+docker exec \
+  -it "odoo.${ODOO_VERSION}_${ODOO_RELEASE}" \
+  bash
---
+++ stop_container.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# #####
+#
+# Stop and remove a running Odoo container.
+#
+# usage:
+#
+#   bash stop_container.sh 9.0 20160101
+#
+# author: Pedro Salgado <steenzout@ymail.com>
+# version: 1.0
+#
+# #####
+
+
+ODOO_VERSION="$1"
+ODOO_RELEASE="$2"
+
+
+validation() {
+  : "${ODOO_VERSION:?first argument needs to be set to the Odoo version (8.0, 9.0 or 10.0).}"
+  : "${ODOO_RELEASE:?second argument needs to be set to the Odoo release (e.g. 20160101).}"
+}
+
+
+validation
+
+docker stop \
+  "odoo.${ODOO_VERSION}_${ODOO_RELEASE}"
+
+docker rm \
+  "odoo.${ODOO_VERSION}_${ODOO_RELEASE}"

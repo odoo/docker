f6b9151aa3e535c7c386b1c6eb787e6f5aed8ee8
Avei20 <muhammadabdulazizalghofari@gmail.com>
2025-04-20 10:21:50 +0700

    fix: Adjust error handling on init.odoo.sh

--- 18.0/init-odoo.sh
+++ 18.0/init-odoo.sh
@@ -6,17 +6,18 @@ export PGPASSWORD=$PASSWORD
 # Wait for PostgreSQL
 sleep 10
 
-query="SELECT latest_version FROM ir_module_module WHERE name = 'base'"
-
-result=$(psql -h $HOST -p $DB_PORT -U $USER -d $DB_NAME -c "$query" -t)
+table_check_query="SELECT to_regclass('public.ir_module_module')"
+table_exists=$(psql -h $HOST -p $DB_PORT -U $USER -d $DB_NAME -t -c "$table_check_query" | xargs)
 
 unset PGPASSWORD
 
 # If the query returns a value, then exit
-if [ -n "$result" ]; then
+if [ "$table_exists" = "ir_module_module" ]; then
     echo "Database already initialized"
     exit 0
 fi
+
+echo "Database is not initialized. Starting initialization..."
 
 # Initialize the database with base module
 odoo --db_host $HOST --db_port $DB_PORT --db_user $USER --db_password $PASSWORD \
